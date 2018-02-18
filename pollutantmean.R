0246dfdd199a38e6ddcf731dc1400a99bdac6233
taylordaldridge <taylordaldridge23@gmail.com>
2018-02-18 15:41:09 -0600

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,14 @@
+#Write a function named 'pollutantmean' that calculates the mean of a pollutant 
+#(sulfate or nitrate) across a specified list of monitors. The function 'pollutantmean' 
+#takes three arguments: 'directory', 'pollutant', and 'id'. 
+#Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' 
+#particulate matter data from the directory specified in the 'directory' 
+#argument and returns the mean of the pollutant across all of the monitors, 
+#ignoring any missing values coded as NA. A prototype of the function is as follows.
+
+
+
+
 #check working directory
 getwd()
 directory<-setwd("/Users/a592006/Desktop/R/specdata")
